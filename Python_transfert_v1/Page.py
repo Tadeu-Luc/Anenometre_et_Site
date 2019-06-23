0ff6147943a172429ae97ff75c30a416fdf5f158
Tadeu Luc <contact@tadeu-luc.com>
2019-06-23 12:59:11 +0200

    Add files via upload

---
+++ Python_transfert_v1/Page.py
@@ -0,0 +1,16 @@
+print(form.getvalue("name"))
+
+html = """<!DOCTYPE html>
+<head>
+    <title>Mon programme</title>
+</head>
+<body>
+    <form action="/index.py" method="post">
+        <input type="text" name="name" value="Votre nom" />
+        <input type="submit" name="send" value="Envoyer information au serveur">
+    </form> 
+</body>
+</html>
+"""
+
+print(html)
---
+++ Python_transfert_v1/install.py
@@ -0,0 +1,19 @@
+# installer for Arduino Weather Station driver
+# Copyright 2017 Alec Bennett
+# Distributed under the terms of the GNU Public License (GPLv3)
+
+from setup import ExtensionInstaller
+
+def loader():
+    return AWSInstaller()
+
+class AWSInstaller(ExtensionInstaller):
+    def __init__(self):
+        super(AWSInstaller, self).__init__(
+            version="0.1",
+            name='aws',
+            description='Collect data from an Arduino connected to a Davis anemometer',
+            author="Alec Bennett",
+            author_email="wrybread@gmail.com",
+            files=[('bin/user', ['bin/user/aws.py'])]
+            )
